258a00d8c3a4ab92274ddf64f99daeb163f820c9
panthercoding <79004908+panthercoding@users.noreply.github.com>
2021-02-27 07:47:55 -0600

    Create CustomLocation.java

---
+++ CustomLocation.java
@@ -0,0 +1,7 @@
+public class CustomLocation{
+  public static void main(String[] args){
+    /* args[0] is a command line argument */
+    Location L = new Location(args[0]);
+    L.printAll();
+  }
+}
